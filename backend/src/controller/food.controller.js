662449c1a5d4b11d64dd4d972969740e6870ebf2
vidhant shriwas <vidhantshrivas1@gmail.com>
2026-03-24 23:22:51 +0530

    video buffer in buffer

--- backend/src/controller/food.controller.js
+++ backend/src/controller/food.controller.js
@@ -1,14 +1,41 @@
 const foodModel = require("../models/food.model")  ;
+ 
+ const storageService = require("../services/storage.service")  ;
 
+  const    { v4:uuid} =require("uuid") ; 
 
-async function createFood(req, res)
-{
+  async function createFood(req, res)
+    {
 
-    console.log(req.foodPartner)  /// it contains all the info about the food partner who is logged in by the request
-  res.status(200).json({message:"create food item "})
-  
+     console.log(req.foodPartner)  
+    
+   
    console.log(req.body)
-   console.log(req.file)  /// it contains the info about the video file which is uploaded by the food partner while creating the food item
-} 
+   console.log(req.file)
 
- module.exports= {createFood}
+   const   fileUploadResult = await storageService.uploadFile(req.file.buffer , uuid());
+ 
+  //   console.log("full result:" ,fileUploadResult)
+  //   console.log(fileUploadResult.url)
+  //  console.log(JSON.stringify(obj, null, 2));
+  //   console.log(resullt)  
+    
+   const foodItem = await foodModel.create({
+    name:req.body.name , 
+    description:req.body.description,
+    video:fileUploadResult.url ,
+    foodPartnerId:req.foodPartner._id
+   })
+
+
+
+ res.status(200).json(
+  {message:"create food item ",
+   food :foodItem
+ })
+      
+  }  
+
+ module.exports= {createFood}  
+
+
